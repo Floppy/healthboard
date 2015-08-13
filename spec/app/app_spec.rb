75b928cb95cf21103088433d2957bf677132faad
James Smith <james@floppy.org.uk>
2015-08-13 22:30:59 +0100

    No need for VCR on app tests, use mock instead

--- spec/app/app_spec.rb
+++ spec/app/app_spec.rb
@@ -1,9 +1,12 @@
-describe HealthBoardApp, :vcr do
+describe HealthBoardApp do
 
   it "should respond to GET" do
+    # Stubs
+    allow(Person.instance).to receive(:steps).and_return(42)
+    # Test
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to match(/Today's steps: /)
+    expect(last_response.body).to match(/Today's steps: 42/)
   end
 
 end
